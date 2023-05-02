0ddcd8f41031bc21e0becb212a652acef87eca34
Sheng-Yu Tsai <stsai@M2-mac.local>
2023-05-01 22:43:11 -0700

    comment out added data

--- db/sql-scripts/InsertData.sql
+++ db/sql-scripts/InsertData.sql
@@ -1,2 +1,9 @@
 INSERT INTO user (id, name, fate) VALUES (0,'plumber johnny', 'johnny exist at present but not in the past');
+-- INSERT INTO user (id, name, fate) VALUES (1,'actress Jane', 'Jane will be a star, shining brightly in the entertainment world.');
+-- INSERT INTO user (id, name, fate) VALUES (2,'gardener Tony', 'Tony will find success in his green thumb and reap the rewards of his labor.');
+-- INSERT INTO user (id, name, fate) VALUES (3,'plumber Nick', 'Nick will find success in his work and be rewarded with a bright future.');
+-- INSERT INTO user (id, name, fate) VALUES (4,'doctor Betty', 'Betty will be a beacon of healing and hope for many.');
+-- INSERT INTO user (id, name, fate) VALUES (5,'lawyer Marshall', 'Marshall will find success in the courtroom and beyond.');
+-- INSERT INTO user (id, name, fate) VALUES (6,'Architect Ted', 'Ted will build a legacy that will last for generations.');
+-- INSERT INTO user (id, name, fate) VALUES (7,'Anchor Robin', 'The future holds great success and joy for you.');
 
